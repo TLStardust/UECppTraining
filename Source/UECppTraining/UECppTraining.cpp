8d62a5908d1b88ba63c4d8a579d26dbc05480b7c
TLStardust <3242340637@qq.com>
2026-01-27 20:37:10 +0800

    Initial Commit: Project Setup

---
+++ Source/UECppTraining/UECppTraining.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UECppTraining.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UECppTraining, "UECppTraining" );
